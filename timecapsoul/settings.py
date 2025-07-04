e6c4783fde766456ece820593cb3594ff1b7a492
Codigo Mantra Pvt. Ltd. <113417843+codigo-mantra@users.noreply.github.com>
2025-07-04 18:13:10 +0530

    Update settings.py

--- timecapsoul/settings.py
+++ timecapsoul/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware', # cors-middleware
@@ -120,11 +121,11 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
- ]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static',
+#  ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
