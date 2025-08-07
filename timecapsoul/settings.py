afcb0a48ccf808cad9c261026bd2a15cb83d6958
Tapendra Kaul <tapendrakaul@gmail.com>
2025-08-07 12:31:02 +0530

    updated migrations and db connection.

--- timecapsoul/settings.py
+++ timecapsoul/settings.py
@@ -18,6 +18,9 @@ import environ
 env = environ.Env()
 environ.Env.read_env()
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -96,10 +99,39 @@ TEMPLATES = [
 # WSGI_APPLICATION = 'timecapsoul.wsgi.application'
 ASGI_APPLICATION = "timecapsoul.asgi.application"
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'timecapsoulDBTEST',
+#         'USER': 'root',
+#         'PASSWORD': 'Test@1234',
+#         'HOST': 'localhost',
+#         'PORT': 3306,
+#         'OPTIONS': {
+#             "init_command": "SET foreign_key_checks = 0;",
+#             'charset': 'utf8mb4',
+#         },
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': AWS_SECRET['DB_NAME'],
+        'USER': AWS_SECRET['DB_USER'],
+        'PASSWORD': AWS_SECRET['DB_PASSWORD'],
+        'HOST': AWS_SECRET['DB_HOST'],
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
