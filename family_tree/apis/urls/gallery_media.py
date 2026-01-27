d40b188d04aeffca34dba15390db4b669ba8ab86
Krishna Yadav <krishnayadav.codigomantra@gmail.com>
2026-01-27 11:50:29 +0530

    gallery api updated

--- family_tree/apis/urls/gallery_media.py
+++ family_tree/apis/urls/gallery_media.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from family_tree.apis.views.gallery_media import ( 
-    FamilyTreeGalaryView
+    FamilyTreeGalaryView, FamilyTreeGallerySearchAPIView
 
 )
 
 urlpatterns = [
-    path('<uuid:family_tree>/gallery/media/list/', FamilyTreeGalaryView.as_view(),),
+    path('<uuid:family_tree_id>/gallery-list/', FamilyTreeGalaryView.as_view(),),
+    path("gallery/search/<uuid:family_tree_id>/", FamilyTreeGallerySearchAPIView.as_view()),
+
 
 ]
